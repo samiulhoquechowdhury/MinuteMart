5f1c94642af80674e1599c9cc436135dbc2107fb
samiulhoquechowdhury <samiulofficialworks@gmail.com>
2026-01-19 21:51:27 +0530

    initial commit

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,11 @@
+import { createContext } from "react";
+
+export const AppContext = createContext();
+
+export const AppContextProvider = ({ children }) => {
+  const value = {};
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
+
+
+export const useAppContext = ()
