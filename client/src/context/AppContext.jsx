18789bce67f5569164a3fda5a3c88d1270e9446a
samiulhoquechowdhury <samiulofficialworks@gmail.com>
2026-02-23 03:21:47 +0530

    basic file and configuration setup

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,11 +1,17 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
-  const value = {};
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+  const [isSeller, setIsSeller] = useState(false);
+  const value = { navigate, user, setUser, setIsSeller, isSeller };
+
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
 
-
-export const useAppContext = ()
+export const useAppContext = () => {
+  return useAppContext(AppContext);
+};
