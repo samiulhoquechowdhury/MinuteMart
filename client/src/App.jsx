1cbf2dc1c7955ab2d61afb0cd7d4e35a1a42ae14
samiulhoquechowdhury <samiulofficialworks@gmail.com>
2026-02-23 04:28:51 +0530

    main banner in progress

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,11 @@ const App = () => {
   return (
     <div>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 };
---
+++ client/src/components/MainBanner.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MainBanner = () => {
+  return <div>MainBanner</div>;
+};
+
+export default MainBanner;
